fc972ae887488f6d9b5b344591aa5114735a2ef9
josephmathew1996 <josephmathew1401@gmail.com>
2018-10-17 17:52:14 +0530

    Add existing file

--- main.go
+++ main.go
@@ -3,23 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	// The "HandleFunc" method accepts a path and a function as arguments
-	// (Yes, we can pass functions as arguments, and even trat them like variables in Go)
-	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
-	http.HandleFunc("/", handler)
+// The new router function creates the router and
+// returns it to us. We can now use this function
+// to instantiate and test the router outside of the main function
 
-	// After defining our server, we finally "listen and serve" on port 8080
-	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
-	// and the handler defined above (in "HandleFunc") is used
-	http.ListenAndServe(":8080", nil)
+func newRouter() *mux.Router {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler).Methods("GET")
+
+	return r
 }
 
-// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
-// as the arguments.
+func main() {
+
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	// For this case, we will always pipe "Hello World" into the response writer
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "Home page!")
+
 }
